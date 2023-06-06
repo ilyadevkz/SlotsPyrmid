b0eb43c1322dde26dd659447e8fd968bb8f58354
ilyazelenskiy <ilya.zelenskiy.020@gmail.com>
2023-06-06 16:28:47 +0300

    test

--- app/src/main/java/com/cricket/masters/ipl/teamstars/slotspyrmid/MainActivity.kt
+++ app/src/main/java/com/cricket/masters/ipl/teamstars/slotspyrmid/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         load_bar = findViewById(R.id.load_bar)
